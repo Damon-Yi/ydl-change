c1b700199585d21f22e4839352da367efc305606
Liyi <yi.li01@bqjr.cn>
2016-12-01 11:38:16 +0800

    --

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.action_p,.action_row,.p_under_line,.link_btn,.link_btn_blank').on('tap',function(){
+        $(this).addClass('active');
+        var _that = this;
+        setTimeout(function(){
+            $(_that).removeClass('active');
+        },200);
+    });
+});
